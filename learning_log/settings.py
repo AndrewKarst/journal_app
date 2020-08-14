465c097ef01e52a55c328f53045ec1563bfe1ce3
Mentat <karst14@gmail.com>
2020-08-13 19:24:58 -0500

    added import os

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -161,6 +162,3 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-    import django_heroku
-    django_heroku.settings(locals())
